4a105cf8e79d15d12a4fbb2dfcad11c8b192628a
salma saadaoui <salma.saadaoui@esprit.tn>
2023-10-16 13:51:08 +0200

    Teeeest1

--- src/main/java/tn/esprit/rh/achat/DTO/ReglementDTO.java
+++ src/main/java/tn/esprit/rh/achat/DTO/ReglementDTO.java
@@ -1,11 +1,9 @@
 package tn.esprit.rh.achat.DTO;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import tn.esprit.rh.achat.entities.Facture;
 
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
 import java.util.Date;
 import lombok.Data;
 
